b7a1e371064b21a45c172c65e863994979ff46fb
Reza Fitriaman <rezafitriaman@gmail.com>
2019-08-25 21:48:10 +0200

    add return on error so the other code dont get executed

--- nodejsCourse/building_restful_apis_using_express_4/express-demo/index.js
+++ nodejsCourse/building_restful_apis_using_express_4/express-demo/index.js
@@ -23,8 +23,7 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
    let course = courses.find(number => number.id === parseInt(req.params.id));
    if(!course) {
-       res.status(404).send('The course with given ID was not found');
-       return;
+       return res.status(404).send('The course with given ID was not found');
    }
    res.send(course);
 });
@@ -34,8 +33,7 @@ app.post('/api/courses', (req, res) => {
 
     const {error} = validateCourse(req.body); // result.error
     if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
 
     const course = {
@@ -50,17 +48,14 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res) => {
 
     let course = courses.find(number => number.id === parseInt(req.params.id));
-    console.log(course);
     if(!course) {
-        res.status(404).send('The course with given ID was not found');
-        return;
+        return res.status(404).send('The course with given ID was not found');
     }
 
     const {error} = validateCourse(req.body);
 
     if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
+        return res.status(400).send(error.details[0].message);
     }
 
     course.name = req.body.name;
